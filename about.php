eb6a973e3b64faa5ce94639fdb254656bb48fa7d
isha-singhal <isinghal40@gmail.com>
2021-12-18 23:37:59 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php
+  include_once 'header.php';
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/about.css">
+  <title>About Us</title>
+</head>
+<body>
+  <div class="about-section">
+    <div class="inner-container">
+      <h1>
+        About Us
+        <div class="border"></div>
+      </h1>
+      <p class="text">
+        I am Isha Singhal, student of Graphic Era Deemed to be University currently pursuing BTech CSE. Comments and improvements regarding this project are more than welcome. To know more about this project, feel free to contact me.
+      </p>
+      <div class="button">
+        <a href="https://www.geu.ac.in/content/geu/en/about.html" target="_blank">Read More</a>
+      </div>
+      <br>
+      <div class="button">
+        <a href="mailto:ishasinghal522@gmail.com">Contact me</a>
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
+
+<?php
+  include_once 'footer.php'; 
+?>
+ 
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+  include_once 'header.php'; 
+?> 
+<!-- Landing page -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/start.css">
+  <title>About Us</title>
+</head>
+<body>
+  <div class="home-section">
+    <div class="inner-container">
+      <h1>
+        Student Result Management System
+        <div class="border"></div>
+      </h1>
+      <p class="text">
+        This is a Student Result Management System portal made with PHP, HTML, CSS and MySQL. In this portal, Students can login via their rollno & class name to get their results and Teachers/Admin can login via their username and password to the SRMS Dashboard where they can perform various activities like add, modify, delete rseults. 
+      </p>
+      <div class="button">
+        <a href="login.php">Login</a>
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
+
+<?php
+  include_once 'footer.php'; 
+?>
+ 
